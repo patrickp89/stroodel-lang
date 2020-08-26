80d529da618737dc1d8def8bb79fdf0907f8585e
patrickp89 <patrickpreuss@netherspace.de>
2020-08-26 17:25:16 +0200

    wip: tokenize Natural numbers

--- Stroodel.Test/UnitTest1.fs
+++ Stroodel.Test/UnitTest1.fs
@@ -17,11 +17,27 @@ let TestAtoms () =
     // TODO: Assert.That(t, Is.EqualTo "(the Atom ''quiche-deluxe)")
 
 [<Test>]
-[<Ignore("Not yet implemented")>]
 let TestTrivialTypes () =
     let code = "sole"
-    let t = TypeChecker.parse code
-    Assert.That(t, Is.EqualTo "(the Trivial sole)")
+    let expr = TypeChecker.parse code
+    Assert.That(expr, Is.EqualTo (Sole))
+    // TODO: let t = TypeChecker.check code
+    // TODO: Assert.That(t, Is.EqualTo "(the Trivial sole)")
+
+[<Test>]
+let TestNaturalNumbers () =
+    let one = "1"
+    let expr = TypeChecker.parse one
+    Assert.That(expr, Is.EqualTo (Nat 1))
+    // TODO: let t = TypeChecker.check one
+    // TODO: Assert.That(t, Is.EqualTo "(the Trivial sole)")
+    let zero = "zero"
+    let actExpr = TypeChecker.parse zero
+    let expExpr = Zero
+    Assert.That(actExpr, Is.EqualTo expExpr)
+    // TODO: let expType = "(the Nat 0)"
+    // TODO: let actType = TypeChecker.check zero
+    // TODO: Assert.That(actType, Is.EqualTo expType)
 
 [<Test>]
 [<Ignore("Not yet implemented")>]
--- Stroodel/Syntax.fs
+++ Stroodel/Syntax.fs
@@ -5,7 +5,19 @@ module Syntax =
     // In the original (Core) grammar, expressions=c and symbols=x
     type Symbol = string
 
+    // Expressions:
     type Expr =
+    | The of (Expr * Expr) // Type annotation
     | VariableReference of Symbol
-    | AtomType
+    | Atom                  // Atom type
     | AtomLiteral of string
+    | Nat of int            // Natural number type
+    | Zero                  // 0
+    | Trivial               // Unit type
+    | Sole                  // Unit constructor
+    | Absurd                // Empty type
+
+    // There also is toplevel syntax for declararations:
+    type Declarations =
+    | Claim
+    | Define
